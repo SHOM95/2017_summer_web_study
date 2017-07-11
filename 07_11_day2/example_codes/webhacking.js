66a6f04afb4b9c2ddb94dbf2afcda5351409913b
Sihwan Oh <sihwan@Sihwanui-MacBook-Pro.local>
2017-07-11 19:11:14 +0900

    README.md 수정

--- 07_11_day2/example_codes/webhacking.js
+++
@@ -1,8 +0,0 @@
-function insertText() {
-    if(document.getElementById('matrix').innerHTML.length <= 100)
-        document.getElementById('matrix').innerHTML += Math.floor(Math.random() + 1);
-}
-
-for(var i = 0; i < 100; i++){
-    
-}
